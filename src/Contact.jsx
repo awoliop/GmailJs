a4f6be00d803c589611a3ddd5eb51bc63fce30ec
awoliop <awoliioop@gmail.com>
2024-01-18 18:27:44 +0300

    inital and final commit

---
+++ src/Contact.jsx
@@ -0,0 +1,40 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+export const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_qenye4p",
+        "template_21wsf3l",
+        form.current,
+        "PUT_YOUR_PUBLIC_KEY_OVER_HERE"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="from_name" />
+      <label>Email</label>
+      <input type="email" name="from_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
+
+export default Contact;
